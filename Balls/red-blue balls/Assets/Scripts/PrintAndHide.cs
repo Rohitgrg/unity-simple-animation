b16846a4db1c0b3ef02a2ab89bc8a31b00ed3713
Rohitgrg <12695856@student.uts.edu.au>
2019-08-21 12:03:53 +1000

    create animation with red and blue balls

---
+++ Balls/red-blue balls/Assets/Scripts/PrintAndHide.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintAndHide : MonoBehaviour
+{
+    public Renderer rend;
+    public int i = 0;
+    public new string name;
+    int rnd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rnd = Random.Range(200, 251);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        name = gameObject.name;
+        Debug.Log(name + ":" + i);
+        if (gameObject.tag == "Red" && i == 100)
+        {
+            gameObject.SetActive(false);
+        }
+        else if (gameObject.tag.Equals("Blue") && i == rnd)
+        {
+            rend.enabled = false;
+        }
+        i++;
+    }
+}
